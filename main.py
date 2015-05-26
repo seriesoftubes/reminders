7bd9cae74683978c2b745479cdd4b971c2ab4143
Brian Weidenbaum <brianweiden@gmail.com>
2015-05-25 20:39:59 -0700

    try to send a txt

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 def _GetAppSecrets():
   with open('secrets.json', 'r') as f:
     return json.loads(f.read())
+_SECRETS = _GetAppSecrets()['twilio']
+
+
+def _GetTwilioRestClient():
+  return twilio.rest.TwilioRestClient(
+    _SECRETS['api_user'], _SECRETS['api_secret'])
 
 
 @app.route('/')
 def Hello():
-  secrets = _GetAppSecrets()
-  return 'Hello secrets! {}'.format(secrets)
+  client = _GetTwilioRestClient()
+  client.messages.create(
+    to='+16313531888', from_=_SECRETS['phone_number'], body='Test')
+  return 'message sent...'
 
 
 @app.errorhandler(404)
