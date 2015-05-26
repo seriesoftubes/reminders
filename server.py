6277c4124f2dcd9cb909eb3b3c0aac8046df5a1b
Brian Weidenbaum <brianweiden@gmail.com>
2015-05-26 09:38:27 -0700

    put people routes behind login wall

--- server.py
+++ server.py
@@ -12,8 +12,8 @@ app.config['DEBUG'] = True
 
 # Routes
 app.add_url_rule('/', view_func=handlers.Hello)
-app.add_url_rule('/create_person', view_func=handlers.CreatePerson)
-app.add_url_rule('/get_people', view_func=handlers.GetAllPeople)
-app.add_url_rule('/delete_person', view_func=handlers.DeletePerson)
-app.add_url_rule('/toggle_is_in_commune', view_func=handlers.ToggleIsInCommune)
-app.add_url_rule('/toggle_can_do_trash', view_func=handlers.ToggleCanDoTrash)
+app.add_url_rule('/people/create', view_func=handlers.CreatePerson)
+app.add_url_rule('/people/list', view_func=handlers.GetAllPeople)
+app.add_url_rule('/people/delete', view_func=handlers.DeletePerson)
+app.add_url_rule('/people/toggle_is_in_commune', view_func=handlers.ToggleIsInCommune)
+app.add_url_rule('/people/toggle_can_do_trash', view_func=handlers.ToggleCanDoTrash)
