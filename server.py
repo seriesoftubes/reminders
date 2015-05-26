2a0e40355a037a36c4be6f5fa2175fd4b585eb60
Brian Weidenbaum <brianweiden@gmail.com>
2015-05-25 21:33:01 -0700

    incl app as a gobal var t omake gae hapy

--- server.py
+++ server.py
@@ -4,16 +4,18 @@ import flask
 import handlers
 
 
-def _ConfigureRoutes(app):
+app = flask.Flask(__name__)
+
+
+def _ConfigureRoutes():
   app.add_url_rule('/', view_func=handlers.Hello)
 
 
 def main():
-  app = flask.Flask(__name__)
   app.config['DEBUG'] = True
   # Note: We don't need to call run() since our application is embedded within
   # the App Engine WSGI application server.
-  _ConfigureRoutes(app)
+  _ConfigureRoutes()
 
 if __name__ == '__main__':
   main()
