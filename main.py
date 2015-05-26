3c31d1538ca3fd331487e685540c8952d8daf38e
Brian Weidenbaum <brianweiden@gmail.com>
2015-05-25 19:30:07 -0700

    Add twilio source (hopefully). Display twilio version in main handler.

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 
-from flask import Flask
+import flask
+import twilio
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
 @app.route('/')
 def Hello():
-  """Return a friendly HTTP greeting."""
-  return 'Hello World!'
+  return 'Hello Twilio version {0}!'.format(twilio.__version__)
 
 
 @app.errorhandler(404)
 def NotFound(e):
   return 'Sorry, nothing at this URL.', 404
-
