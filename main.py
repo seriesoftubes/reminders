4bafe28b13ecac3feff9dd9aa3b620f283edf8d4
Brian Weidenbaum <brianweiden@gmail.com>
2015-05-25 20:47:24 -0700

    also send email

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import json
 import flask
 import twilio
 
+from google.appengine.api import mail
 
+
+# TODO: try to put this stuff into if __name__ == '__main__'
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
@@ -14,6 +17,8 @@ def _GetAppSecrets():
   with open('secrets.json', 'r') as f:
     return json.loads(f.read())
 _SECRETS = _GetAppSecrets()['twilio']
+
+_EMAIL_ADDRESS = 'reminders@commune-2-0.com'
 
 
 def _GetTwilioRestClient():
@@ -26,6 +31,11 @@ def Hello():
   client = _GetTwilioRestClient()
   client.messages.create(
     to='+16313531888', from_=_SECRETS['phone_number'], body='Test')
+  from_address = _EMAIL_ADDRESS
+  to_address = 'brianweiden@gmail.com'
+  subject = 'TEST SUBJECT!'
+  body = 'Test body!'
+  mail.send_mail(from_address, to_address, subject, body)
   return 'message sent...'
 
 
